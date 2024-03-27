e2bd79226f12809392e9d7e1de78db38beeb5db8
Shahryar <muhammad.shahryar@braincrop.net>
2024-03-27 14:29:54 +0500

    Feat:Image Upload Feature

---
+++ Practice.API/Controllers/ImageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Practice.API.Models.DTO;
+
+namespace Practice.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        public ImageController()
+        {
+            
+        }
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto uploadRequestDto)
+        {
+
+        }
+    }
+}
---
+++ Practice.API/Models/DTO/ImageUploadRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practice.API.Models.DTO
+{
+    public class ImageUploadRequestDto
+    {
+        [Required]
+        public IFormFile File { get; set; }
+        [Required]
+        public string Filename { get; set; }
+        public string? Filedescription { get; set; }
+
+
+    }
+}
